ad2551525d9192ce96cf9486137fc488af488f31
Kalum <kshorty@live.co.uk>
2019-01-30 11:51:42 +0000

    cleaned up the selector page

--- src/component/Cats.js
+++ src/component/Cats.js
@@ -15,43 +15,18 @@ class Cats extends Component {
 
     render(){
         const { cats } = this.props;
-        // const options = [
-        //     <div>
-        //     {cats.map((item) => (
-        //         {
-        //             value: item.name,
-        //             label: item.name,
-                  
-
-        //         }
-        //     ))}
-        //     </div>
-        // ]
+       
         return(
             <>
                 <div>
                     <h2>Cats</h2>
                 </div>
-                {/* <div>
-                    {cats.map((item, index)=> (
-                        <Cat
-                            name={item.name}
-                            key={index}
-                        />
-                    ))}
-                </div> */}
                 <div>
                     <Select options = {cats.map((item, index) => (
                         {
                             value: item.name,
                             label: item.name
                         }
-                        // <Cat
-                        //     name={item.name}
-                        //     key={index}
-                        // />
-
-                        
                     ))}/>
                 </div>
             </>
