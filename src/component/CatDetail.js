e56e023a9df0c588a5791d9af13e353ab27d2e36
Kalum <kshorty@live.co.uk>
2019-02-01 15:18:31 +0000

    fully styled the cat detail page

--- src/component/CatDetail.js
+++ src/component/CatDetail.js
@@ -9,24 +9,30 @@ class CatDetail extends Component {
        
         return(
             <>
-                <div className="display-cat-container">
+                {DisplayCat.origin?
+                    <div className="display-cat-container">
 
-                    <div className="display-cat-top-info">
-                        <h1 className="display-cat-name">{DisplayCat.name}</h1>
-                        <p className="display-cat-desciption">{DisplayCat.description}</p>
-                        <h3 className="display-cat-origin">{DisplayCat.origin}</h3>
-                        <h2 className="displlay-cat-temper">{DisplayCat.temperament}</h2>
-                    </div>
+                        <div className="display-cat-top-info">
+                            <h1 className="display-cat-name">{DisplayCat.name}</h1>
+                            <p className="display-cat-desciption">{DisplayCat.description}</p>
+                            <h3 className="display-cat-origin">  <span className="text-label">Origin: </span><br></br>{DisplayCat.origin}</h3>
+                            <h2 className="display-cat-temper"> <span className="text-label">Temperament: </span><br></br>{DisplayCat.temperament}</h2>
+                        </div>
 
-                    <div className="display-cat-cubes">
-                        <h2 className="display-cat-cube">{DisplayCat.affection_level}</h2>
-                        <h2 className="display-cat-cube">{DisplayCat.child_friendly}</h2>
-                        <h2 className="display-cat-cube">{DisplayCat.energy_level}</h2>
-                        <h2 className="display-cat-cube">{DisplayCat.intelligence}</h2>
-                        <h2 className="display-cat-cube">{DisplayCat.life_span}</h2>
-                        <h2 className="display-cat-cube">{DisplayCat.social_needs}</h2>
+                        <div className="display-cat-cubes">
+                            <h2 className="display-cat-cube"> <span className="text-label">Affection Level:</span> <br></br>{DisplayCat.affection_level}</h2>
+                            <h2 className="display-cat-cube">  <span className="text-label">Child Friendliness: </span><br></br>{DisplayCat.child_friendly}</h2>
+                            <h2 className="display-cat-cube">  <span className="text-label">Energy Level: </span> <br></br>{DisplayCat.energy_level}</h2>
+                            <h2 className="display-cat-cube">  <span className="text-label">Intelligence: </span><br></br> {DisplayCat.intelligence}</h2>
+                            <h2 className="display-cat-cube">  <span className="text-label">Life-Span: </span><br></br>{DisplayCat.life_span}<span className="text-label"> Years</span> </h2>
+                            <h2 className="display-cat-cube">  <span className="text-label">Social Needs: </span><br></br>{DisplayCat.social_needs}</h2>
+                        </div>
                     </div>
-                </div>
+                    :
+                    <div>
+                        <h1>please choose a breed</h1>
+                    </div>
+                }
             </>
         )
     }
--- src/component/Cats.js
+++ src/component/Cats.js
@@ -29,7 +29,7 @@ class Cats extends Component {
                 </div>
                 <div className="cat-selector-field">
                     <Select
-                    className="selector"
+                    className="cat-selector"
                     onChange={this.onChange.bind(this)}
                      options = {cats.map((item) => (
                         {
