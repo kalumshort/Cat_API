def657350c8e0abb5c8ea50876750554b47e3884
Kalum <kshorty@live.co.uk>
2019-02-01 13:17:31 +0000

    added all of the data on the detail page

--- src/component/CatDetail.js
+++ src/component/CatDetail.js
@@ -10,8 +10,21 @@ class CatDetail extends Component {
         return(
             <>
                 <div className="display-cat-container">
-                    <div>
-                        <h2 className="display-cat-name">{DisplayCat.name}</h2>
+
+                    <div className="display-cat-top-info">
+                        <h1 className="display-cat-name">{DisplayCat.name}</h1>
+                        <p className="display-cat-desciption">{DisplayCat.description}</p>
+                        <h3 className="display-cat-origin">{DisplayCat.origin}</h3>
+                        <h2 className="displlay-cat-temper">{DisplayCat.temperament}</h2>
+                    </div>
+
+                    <div className="display-cat-cubes">
+                        <h2 className="display-cat-cube">{DisplayCat.affection_level}</h2>
+                        <h2 className="display-cat-cube">{DisplayCat.child_friendly}</h2>
+                        <h2 className="display-cat-cube">{DisplayCat.energy_level}</h2>
+                        <h2 className="display-cat-cube">{DisplayCat.intelligence}</h2>
+                        <h2 className="display-cat-cube">{DisplayCat.life_span}</h2>
+                        <h2 className="display-cat-cube">{DisplayCat.social_needs}</h2>
                     </div>
                 </div>
             </>
